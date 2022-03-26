d7d4f3f29518597eebfb958725c040c13c5f36bc
Andre Lee <customscriptudg@gmail.com>
2022-03-26 12:24:34 +0800

    update friends page
    improve UI, add "calories" and "steps' fields in database

--- lib/screens/historicalsite.dart
+++ lib/screens/historicalsite.dart
@@ -12,7 +12,6 @@ import 'package:history_buddy/HistSite.dart';
 /// add images to each historical site if possible
 /// add button to link to navigator to open map
 class historicalsite extends StatefulWidget {
-  
   static List<HistSite> sortedHistSites = [];
   static List<GeoJsonPoint> points = [];
   @override
@@ -30,8 +29,8 @@ class _historicalsiteState extends State<historicalsite> {
     final geo = GeoJson();
 
     List dummy_data = [];
-    final String s = await DefaultAssetBundle.of(context).loadString(
-        'assets/historic-sites-geojson.geojson');
+    final String s = await DefaultAssetBundle.of(context)
+        .loadString('assets/historic-sites-geojson.geojson');
     await geo.parse(s, verbose: true);
     final data = json.decode(s);
 
@@ -42,35 +41,39 @@ class _historicalsiteState extends State<historicalsite> {
     String info = data["features"][0]["properties"]["Description"];
     var name_index = info.indexOf("NAME");
     var searchString = r"<";
-    var stop_nameindex = info.indexOf(searchString,name_index+13);
-    String siteName = info.substring(name_index+14, stop_nameindex);
+    var stop_nameindex = info.indexOf(searchString, name_index + 13);
+    String siteName = info.substring(name_index + 14, stop_nameindex);
     print(siteName);
     var start_descindex = info.indexOf("DESCRIPTION");
     var searchString1 = r"<";
-    var stop_descindex = info.indexOf(searchString1,start_descindex+20);
-    String desc = info.substring(start_descindex+21, stop_descindex);
+    var stop_descindex = info.indexOf(searchString1, start_descindex + 20);
+    String desc = info.substring(start_descindex + 21, stop_descindex);
     print(desc);
     //List<String> histnames = [];
     //List<LatLng> coords = [];
     //List<String> desc = [];
     //List<HistSite> histsitelist = [];
-     for (int i = 0; i < data["features"].length; i++) {
-       String info = data["features"][i]["properties"]["Description"];
-       var name_index = info.indexOf("NAME");
-       var stop_nameindex = info.indexOf(r"<",name_index+13);
-       String siteName = info.substring(name_index+14, stop_nameindex);
-       var start_descindex = info.indexOf("DESCRIPTION");
-       var stop_descindex = info.indexOf(r"<",start_descindex+20);
-       String desc = info.substring(start_descindex+21, stop_descindex);
-     //  String siteName = data["features"][i]["properties"]["Name"];
-     // histnames.add(data["features"][i]["properties"]["Name"]);
+    for (int i = 0; i < data["features"].length; i++) {
+      String info = data["features"][i]["properties"]["Description"];
+      var name_index = info.indexOf("NAME");
+      var stop_nameindex = info.indexOf(r"<", name_index + 13);
+      String siteName = info.substring(name_index + 14, stop_nameindex);
+      var start_descindex = info.indexOf("DESCRIPTION");
+      var stop_descindex = info.indexOf(r"<", start_descindex + 20);
+      String desc = info.substring(start_descindex + 21, stop_descindex);
+      //  String siteName = data["features"][i]["properties"]["Name"];
+      // histnames.add(data["features"][i]["properties"]["Name"]);
 
       //coords.add(LatLng(data["features"][i]["geometry"]["coordinates"][0],
-        //  data["features"][i]["geometry"]["coordinates"][1]));
+      //  data["features"][i]["geometry"]["coordinates"][1]));
       //desc.add(data["features"][i]["properties"]["Description"]);
-        HistSite histsite =  HistSite(siteName, data["features"][i]["geometry"]["coordinates"][0],
-           data["features"][i]["geometry"]["coordinates"][1], desc, 0);
-        HistSiteList.add(histsite);
+      HistSite histsite = HistSite(
+          siteName,
+          data["features"][i]["geometry"]["coordinates"][0],
+          data["features"][i]["geometry"]["coordinates"][1],
+          desc,
+          0);
+      HistSiteList.add(histsite);
     }
   }
 
@@ -115,15 +118,15 @@ class _historicalsiteState extends State<historicalsite> {
   void asyncLoad() async {
     final geo = GeoJson();
     Position position = await _determinePosition();
-     await readSiteLocations();
+    await readSiteLocations();
 
     //final String s = await rootBundle.loadString(
-     //   'assets/historic-sites-geojson.geojson');
-   // await geo.parse(s, verbose: true);
+    //   'assets/historic-sites-geojson.geojson');
+    // await geo.parse(s, verbose: true);
 
-
- //   final List<GeoJsonPoint> points = geo.points;
-    historicalsite.sortedHistSites = SitesData.filterSitesByDistance(HistSiteList, position.latitude, position.longitude);
+    //   final List<GeoJsonPoint> points = geo.points;
+    historicalsite.sortedHistSites = SitesData.filterSitesByDistance(
+        HistSiteList, position.latitude, position.longitude);
   }
 
   // initialise the state of the UI
@@ -137,44 +140,40 @@ class _historicalsiteState extends State<historicalsite> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
-      body: NestedScrollView(
-          floatHeaderSlivers: true,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                elevation: 0,
-                backgroundColor: Colors.teal[200],
-                title: Text("Historical Sites", style: TextStyle(fontSize: 22, color: Colors.white)),
-                centerTitle: true,
-                expandedHeight: 60.0,
-                floating: false,
-                pinned: true,
-
-
-              ),
-            ];
-          },
-
-          body: ListView.builder(
-            itemCount: historicalsite.sortedHistSites.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: Icon(Icons.hiking, color: Colors.teal[100], size: 40),
-                title: Text(historicalsite.sortedHistSites[index].getName()),
-                subtitle: Text(historicalsite.sortedHistSites[index].getDesc() + '\n' + historicalsite.sortedHistSites[index].getDist().toString()),
-
-              );
-            }
-
-              )
-                )
-              );
-
+        backgroundColor: Colors.white70,
+        body: NestedScrollView(
+            floatHeaderSlivers: true,
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  elevation: 0,
+                  backgroundColor: Colors.teal[200],
+                  title: Text("Historical Sites",
+                      style: TextStyle(fontSize: 22, color: Colors.white)),
+                  centerTitle: true,
+                  expandedHeight: 60.0,
+                  floating: false,
+                  pinned: true,
+                ),
+              ];
+            },
+            body: ListView.builder(
+                itemCount: historicalsite.sortedHistSites.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading:
+                        Icon(Icons.hiking, color: Colors.teal[100], size: 40),
+                    title:
+                        Text(historicalsite.sortedHistSites[index].getName()),
+                    subtitle: Text(
+                        historicalsite.sortedHistSites[index].getDesc() +
+                            '\n' +
+                            historicalsite.sortedHistSites[index]
+                                .getDist()
+                                .toString()),
+                  );
+                })));
   }
-
-
-
-
-
 }
